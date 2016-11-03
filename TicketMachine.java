5c97821e5a42a360f0834f8cbf84b969754a4a75
ivanozil11 <ivancalfer1@gmail.com>
2016-11-03 11:51:40 +0100

    0456.a, ahora hay dos maquinas una con premio y otra sin el

--- TicketMachine.java
+++ TicketMachine.java
@@ -17,15 +17,16 @@ public class TicketMachine
     private int balance;
     // The total amount of money collected by this machine.
     private int total;
+    private int prize;
 
     /**
      * Create a machine that issues tickets of the given price.
      */
-    public TicketMachine(int cost)
+    public TicketMachine(int cost, int prem)
     {
         price = cost;
-        balance = 0;
-        total = 0;
+        balance = 0;total = 0;
+        prem= prize;
     }
 
     /**
@@ -65,10 +66,15 @@ public class TicketMachine
      * reduce the current balance by the ticket price. Print
      * an error message if more money is required.
      */
-    public void printTicket()
-    {
-        if(balance >= price) {
-            // Simulate the printing of a ticket.
+    public void printTicket(){    
+       if (prize == 1){
+          System.out.println("##################");
+            System.out.println("# The BlueJ Line");
+            System.out.println("# Ticket");
+            System.out.println("# " + price + " cents.");
+            System.out.println("##################");
+            System.out.println();
+
             System.out.println("##################");
             System.out.println("# The BlueJ Line");
             System.out.println("# Ticket");
@@ -80,41 +86,29 @@ public class TicketMachine
             total = total + price;
             // Reduce the balance by the prince.
             balance = balance - price;
+
+            // Update the total collected with the price.
+            total = total + price;
+            // Reduce the balance by the prince.
+            balance = balance - price;
+            
         }
-        else {
-            System.out.println("You must insert at least: " +
-                               (price - balance) + " more cents.");
+        else  {
+            System.out.println("##################");
+            System.out.println("# The BlueJ Line");
+            System.out.println("# Ticket");
+            System.out.println("# " + price + " cents.");
+            System.out.println("##################");
+            System.out.println();
                     
         }
+      
+      
     }
 
-    /**
-     * Return the money in the balance.
-     * The balance is cleared.
-     */
-    public int refundBalance()
-    {
-        int amountToRefund;
-        amountToRefund = balance;
-        balance = 0;
-        return amountToRefund;
-    }
-    public int emptyMachine()
-   {
-   if (balance == 0){    
-       
-       int amountToRefund;
-       amountToRefund= balance;
-       balance=0;
-       return amountToRefund;
-       
-    }
+    
+    } 
 
-else {
-    return -1;
 
-}
-}
-}
 
 
